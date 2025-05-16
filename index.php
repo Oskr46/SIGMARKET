c2cbba9ed526ae5d9babcb3e7909cba5bc14b0bd
Oskr46 <oscarraf64@gmail.com>
2025-05-16 17:32:59 -0400

    Prototipo del Header y Estilos

--- index.php
+++ index.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SIGMARKET</title>
+    <link rel="stylesheet" href="styles/global.css">
 </head>
 <body>
+    <header class="header">
+        <div class="items">
+            <a href="/">SIGMARKET</a>
+            <a href="./components/products">Productos</a>
+            <input class="input" placeholder="Busca un Producto"></input>
+            <button class="search">Buscar<img src=''></img></button>
+        </div>
+    </header>
     
 </body>
 </html>
