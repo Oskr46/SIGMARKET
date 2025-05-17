d4fad9566038ffa9a5d8be6c53e317c1332c8111
Oskr46 <oscarraf64@gmail.com>
2025-05-17 17:47:31 -0400

    Mejora en la implementacion de Header y Footer a traves de funciones con PHP

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require('./components/header_footer.php')?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -9,25 +10,7 @@
 </head>
 <body>
     <header class="header">
-        <div class="header_content">
-            <div class="logo">
-                <a href="/"><img src="./res/img/logo_smkt.png"></img></a>
-            </div>
-
-            <a class="item" href="./components/products">Productos</a>
-            
-            <a class="item" href=''>En venta</a>
-            
-            <a class="item" href=''>Marcas</a>
-            
-            <button class="search"></button>
-            
-            <input class="search_input" placeholder="Busca un Producto"></input>
-            
-            <a class="sign_in" href="">Registrate Ahora</a>
-            
-            <a class="sign_up" href="">Iniciar Sesion</a>
-        </div>
+        <?php show_header(); ?>
     </header>
 
     <div class="main_cont">
@@ -41,25 +24,7 @@
     </div>
     
     <footer class="footer">
-        <div class="footer_content">
-            <div class="logo_and_info">
-                <img class="logo" src="./res/img/logo_smkt.png" alt="Logo de la tienda"></img>
-                <p class='info'>Tenemos prendas que se adaptan a tu estilo y que te enorgulleces de llevar. 
-                Desde ropa de mujer hasta de hombre.</p>
-            </div>
-
-            <div class="other_items">
-                <h2>AYUDA</h2>
-                <a class="items_footer" href=''>Terminos y condiciones</a>
-                <a class="items_footer" href=''>Politica de Privacidad</a>
-            </div>
-            <div class="other_items">
-                <h2>FAQ</h2>
-                <a class="items_footer" href=''>Cuenta</a>
-                <a class="items_footer" href=''>Ordenes</a>
-                <a class="items_footer" href=''>Pagos</a>
-            </div>
-        </div>
+        <?php show_footer()?>
     </footer>
 </body>
 </html>
