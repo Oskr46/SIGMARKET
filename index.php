0c7133c2fca5e68fb3037d6da7e27296e2fcfc0f
Oskr46 <oscarraf64@gmail.com>
2025-05-17 15:27:18 -0400

    Agregado del Mensaje de Bienvenida

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Principal - SIGMARKET</title>
     <link rel="stylesheet" href="styles/global.css">
-    <link rel="icon" href="res/img/favicon.png" type="image/x-icon">
+    <link rel="icon" href="res/img/favicon_white.png" type="image/x-icon">
 </head>
 <body>
     <header class="header">
@@ -13,48 +13,30 @@
             <div class="logo">
                 <a href="/"><img src="./res/img/logo_smkt.png"></img></a>
             </div>
+
             <a class="item" href="./components/products">Productos</a>
+            
             <a class="item" href=''>En venta</a>
+            
             <a class="item" href=''>Marcas</a>
+            
             <button class="search"></button>
+            
             <input class="search_input" placeholder="Busca un Producto"></input>
+            
             <a class="sign_in" href="">Registrate Ahora</a>
+            
             <a class="sign_up" href="">Iniciar Sesion</a>
         </div>
     </header>
 
-    <div>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
-        <h1>AAA</h1>
+    <div class="main_cont">
+        <h2 class="welcome_msg_title">Conviertete en un insano con Sigma</h2>
+        <p class='welcome_msg_detail'>
+            Explore nuestra variada gama de prendas meticulosamente elaboradas,
+            diseñadas para resaltar su individualidad y satisfacer su sentido del estilo.
+        </p>
+        <button class="welcome_button">Empieza a comprar ahora</button>
     </div>
     
 </body>
