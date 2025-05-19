c229eea46052d85045cbc66e621d8953be0179ef
Oskr46 <oscarraf64@gmail.com>
2025-05-19 14:10:15 -0400

    Agregado de modulo de Registro + Arreglos de redireccionamiento en Header y Footer

--- components/header_footer.php
+++ components/header_footer.php
@@ -1,6 +1,6 @@
 <?php
+    require_once(__DIR__ . '/../config.php');
     session_start();
-    require('./styles/global.css');
     
     if(isset($_SESSION['admin']) && isset($_SESSION['name']) && isset($_SESSION['email'])){
         $admin = $_SESSION['admin'];
@@ -12,21 +12,21 @@
 ?>
     <div class="header_content">
             <div class="logo">
-                <a href="/"><img src="./res/img/logo_smkt.png"></img></a>
+                <a href="<?php echo BASE_URL; ?>"><img src="<?php echo BASE_URL;?>/res/img/logo_smkt.png"></img></a>
             </div>
-            <a class="item" href="./components/products">Productos</a>
-            <a class="item" href=''>En venta</a>
-            <a class="item" href=''>Marcas</a>
+            <a class="item" href="<?php BASE_URL ?> ./components/products">Productos</a>
+            <a class="item" href="<?php BASE_URL ?> ">En venta</a>
+            <a class="item" href="<?php BASE_URL ?> ">Marcas</a>
             <button class="search"></button>
             <input class="search_input" placeholder="Busca un Producto"></input>
-            <?php if(isset($admin) && !isset($name) && !isset($email)){?>
-            <a class="sign_in" href="">Registrate Ahora</a>
+            <?php if(!isset($admin) && !isset($name) && !isset($email)){?>
+            <a class="sign_in" href="<?php BASE_URL ?> pages/register_page.php">Registrate Ahora</a>
             
-            <a class="sign_up" href="">Iniciar Sesion</a>
+            <a class="sign_up" href="<?php BASE_URL ?> ">Iniciar Sesion</a>
             <?php }
             else{?>
-            <a class="cart" href=""><img src="../res/img/cart_icon.png"/></a>
-            <a class="user_settings" href=""><img src="../res/img/user_icon.png"/></a>
+            <a class="cart" href=""><img src="<?php BASE_URL ?> ../res/img/cart_icon.png"/></a>
+            <a class="user_settings" href=""><img src="<?php BASE_URL ?> ../res/img/user_icon.png"/></a>
             <?php }?>
         </div>
 <?php 
@@ -35,7 +35,7 @@
     function show_footer(){?>
         <div class="footer_content">
             <div class="logo_and_info">
-                <img class="logo" src="./res/img/logo_smkt.png" alt="Logo de la tienda"></img>
+                <img class="logo" src="<?php BASE_URL ?> ../res/img/logo_smkt.png" alt="Logo de la tienda"></img>
                 <p class='info'>Tenemos prendas que se adaptan a tu estilo y que te enorgulleces de llevar. 
                 Desde ropa de mujer hasta de hombre.</p>
             </div>
---
+++ pages/register_page.php
@@ -0,0 +1,50 @@
+<?php require('../components/header_footer.php')?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro - SIGMARKET</title>
+    <link rel= "icon" href="../res/img/favicon_white.png"/>
+    <link rel="stylesheet" href="../styles/global.css"/>
+    <link rel="stylesheet" href="../styles/register.css"/>
+</head>
+<body>
+    <header class="header">
+        <?php show_header();?>
+    </header>
+
+        <div class="register">
+            <div class ="register_title">
+                <h2>Registro de Usuario</h2>
+            </div>
+
+            <div class ="register_container">
+                <div class="register_container_title">
+                    <h3>Completa la informacion para crear tu cuenta</h3>
+                    <div class="register_container_data">
+                        <label for="email">Correo Electronico</label>
+                        <input name= "email" type="text" placeholder="Email"/>
+                    </div>
+                    <div class="register_container_data">
+                        <label for="name">Nombre</label>
+                        <input name= "name" type="text" placeholder="Coloca tu nombre"/>
+                    </div>
+                    <div class="register_container_data">
+                        <label for="phone">Numero Telefónico</label>
+                        <input name= "phone" type="text" placeholder="Coloca tu número telefónico"/>
+                    </div>
+                    <div class="register_container_data">
+                        <label for="password">Contraseña</label>
+                        <input name= "password" type="text" placeholder="Crea una Contraseña"/>
+                    </div>
+                    </div> 
+                </div>
+            </div> 
+        </div>
+
+    <footer class="footer">
+        <?php show_footer(); ?>
+    </footer>
+</body>
+</html>
