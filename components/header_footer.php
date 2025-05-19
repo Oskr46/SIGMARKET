2fb249d381b781168ed3da71f3a84b8b192bd9a9
Oskr46 <oscarraf64@gmail.com>
2025-05-19 10:55:55 -0400

    Agregado de Header dinamico para usuario y visitante

--- components/header_footer.php
+++ components/header_footer.php
@@ -1,25 +1,33 @@
 <?php
+    session_start();
     require('./styles/global.css');
+    
+    if(isset($_SESSION['admin']) && isset($_SESSION['name']) && isset($_SESSION['email'])){
+        $admin = $_SESSION['admin'];
+        $name = $_SESSION['name'];
+        $email = $_SESSION['email']; 
+    }
+    
     function show_header(){
 ?>
     <div class="header_content">
             <div class="logo">
                 <a href="/"><img src="./res/img/logo_smkt.png"></img></a>
             </div>
-
             <a class="item" href="./components/products">Productos</a>
-            
             <a class="item" href=''>En venta</a>
-            
             <a class="item" href=''>Marcas</a>
-            
             <button class="search"></button>
-            
             <input class="search_input" placeholder="Busca un Producto"></input>
-            
+            <?php if(isset($admin) && !isset($name) && !isset($email)){?>
             <a class="sign_in" href="">Registrate Ahora</a>
             
             <a class="sign_up" href="">Iniciar Sesion</a>
+            <?php }
+            else{?>
+            <a class="cart" href=""><img src="../res/img/cart_icon.png"/></a>
+            <a class="user_settings" href=""><img src="../res/img/user_icon.png"/></a>
+            <?php }?>
         </div>
 <?php 
 }
